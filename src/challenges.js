9c6178aa4588c1956c2be4377985ffe912290c4d
Leonardo C Araújo <ogbeni-leonardo@outlook.com>
2022-04-12 15:01:23 -0300

    Resolução dos desafios 3 e 4

--- src/challenges.js
+++ src/challenges.js
@@ -9,13 +9,13 @@ function calcArea(base, altura) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  return frase.split(' ');
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
 
 // Desafio 5
