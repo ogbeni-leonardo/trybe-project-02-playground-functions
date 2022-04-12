d1fae9240ad2b71077550bed052c8e5daece8922
Leonardo C Araújo <ogbeni-leonardo@outlook.com>
2022-04-12 17:00:15 -0300

    Resolução dos desafios 7 e 8

--- src/challenges.js
+++ src/challenges.js
@@ -28,34 +28,61 @@ function highestCount(array) {
   let bigger;
   let repeat;
 
-  for (let index in array) {
-    number = array[index];
-    if (index === '0') {
+  for (let index = 0; index < array.length; index += 1) {
+    let number = array[index];
+    if (index === 0 || number > bigger) {
       bigger = number;
       repeat = 1;
-    } 
-    else if (number == bigger) {
+    } else if (number === bigger) {
       repeat += 1;
-    } 
-    else if (number > bigger) {
-      bigger = number;
-      repeat = 1;
     }
   }
-  
   return repeat;
 }
 
-console.log(highestCount([0, 0, 0]))
-
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distance1 = mouse - cat1; // -1
+  let distance2 = mouse - cat2; // -1
+  // Se a distância que existe entre o rato, o gato 1 e o gato 2 for positiva
+  if (distance1 > 0 && distance2 > 0) {
+    // Se a distância for a mesma
+    if (distance1 === distance2) {
+      return 'os gatos trombam e o rato foge';
+    } else if (distance1 < distance2) {
+      return 'cat 1';
+    }
+    // Caso nenhuma das afirmações acima tenham sido verdadeiras
+    return 'cat2';
+  // Se um dos valores de distância for negativo
+  } else {
+    // Se a soma do(s) valor(es) for igual a zero (distância igual)
+    if (distance1 + distance2 === 0 || distance1 === distance2) {
+      return 'os gatos trombam e o rato foge';
+    } else if (distance1 > distance2) {
+      return 'cat1'
+    }
+    // Caso nenhuma das afirmações acima tenham sido verdadeiras
+    return 'cat2'
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let result = [];
+
+  for (let number of array) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      result.push('fizzBuzz');
+    } else if (number % 3 === 0) {
+      result.push('fizz');
+    } else if (number % 5 === 0) {
+      result.push('buzz')
+    } else {
+      result.push('bug!');
+    }
+  }
+  return result;
 }
 
 // Desafio 9
